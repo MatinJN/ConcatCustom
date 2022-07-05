f5e7f38cad2b9139ab549afbd7a93e76b19157a1
MeteHan96 <101628155+MeteHan96@users.noreply.github.com>
2022-07-05 18:10:22 +0400

    Add files via upload

---
+++ 1.ConcatCustom/app.js
@@ -0,0 +1,23 @@
+// let Boys = ['Serxan','Matin','Murad'];
+// let Girls = ['Aysel','Gunay','Lala']
+// let guys = []
+
+// Boys.forEach(element => {
+//     guys.push(element)
+// });
+
+// Girls.forEach(element => {
+//     guys.push(element)
+// });
+
+// console.log(guys);
+
+let vaxt = new Date().getHours();
+console.log(vaxt);
+
+if (vaxt > 5 && vaxt <  13) {
+    alert('Sabahin xeyir')
+}
+else if (vaxt > 12 && vaxt <  23) {
+    alert('axsamin xeyir')
+} 
